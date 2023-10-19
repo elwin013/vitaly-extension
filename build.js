6ad9c04cf851f843fc7b4a3d39560cc7b50d70a7
Kamil Banach <kontakt@elwin013.com>
2023-10-19 10:59:36 +0200

    Update build.js to work with type=module

--- build.js
+++ build.js
@@ -1,28 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-var crx3 = require('crx3'),
-  path = require('path'),
-  fs = require('fs'),
-  packageJson = require('./package.json');
+import crx3 from 'crx3';
+import { join } from 'path';
+import { existsSync, rmSync } from 'fs';
+import packageJson from './package.json' assert { type: 'json' };
 
-let manifestPath = path.join('./dist', 'manifest.json');
+let manifestPath = join('./dist', 'manifest.json');
 const name = packageJson.name;
 
 console.log(`Building '${name}' with manifest from ${manifestPath}`);
 
 try {
-  const extensionPath = path.join('dist', name + '.crx');
-  if (fs.existsSync(extensionPath)) {
-    fs.rmSync(extensionPath);
+  const extensionPath = join('dist', name + '.crx');
+  if (existsSync(extensionPath)) {
+    rmSync(extensionPath);
   }
   crx3([manifestPath], {
-    keyPath: path.join('secrets', 'private-key.pem'),
+    keyPath: join('secrets', 'private-key.pem'),
     crxPath: extensionPath,
   }).then(function () {
-    console.log(
-      'Extension package written to ' + path.join('dist', name + '.crx')
-    );
+    console.log('Extension package written to ' + join('dist', name + '.crx'));
   });
 } catch (errorMsg) {
   console.error(`Error: ${errorMsg}s`);
-  return 1;
 }
